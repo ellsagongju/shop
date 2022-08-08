224755eebbe2111180b0aa9853b4b107329b3f56
hyojin jo <mlhj6010@naver.com>
2022-08-08 22:05:23 +0900

    쇼핑몰 첫 페이지 npx로 받고 부트스트랩 했음

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { Navbar,Container,Nav } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">shop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  )
+}
+
+export default App;
