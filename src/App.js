6c92911211b4559854cfb3403e6a5d721538b355
hyojin jo <mlhj6010@naver.com>
2022-08-09 22:00:52 +0900

    이미지 삽입, public 폴더 이미지는 압축 안됨.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Navbar, Container, Nav,Row,Col } from 'react-bootstrap';
 import bg from './img/small_event4.jpg';
 
+// 이미지를 쓸 때 public 폴더를 쓸 수 있다.
+// build하게 되면 src는 압축이 되지만
+// public은 압축되지 않기 때문에,
+// '/'절대경로로 쓰면 된다.
 function App() {
   return (
     <div>
@@ -16,8 +20,31 @@ function App() {
         </Container>
       </Navbar>
 
-      <div className='main_bg'></div>
-      <div className='main_bg' style={{backgroundImage:'url('+ bg +')'}}></div>
+      {/* <div className='main_bg'></div> */}
+      <div className='main_bg' style={{ backgroundImage: 'url(' + bg + ')' }}></div>
+      
+      <Row>
+        <Col xs={12} md={4}>
+          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width='80%'></img>
+          {/* public 폴더에서 이미지 가져올 때, 권장사항 */}
+          {/* <img src={ process.env.PUBLIC_URL + '/img/logo192.png'} width='80%'></img> */}
+          <h3>신발1</h3>
+          <span>태그</span>
+          <p>가격</p>
+        </Col>
+        <Col xs={12} md={4}>
+          <img src="https://codingapple1.github.io/shop/shoes2.jpg" width='80%'></img>
+          <h3>신발2</h3>
+          <span>태그</span>
+          <p>가격</p>
+        </Col>
+        <Col xs={12} md={4}>
+          <img src="https://codingapple1.github.io/shop/shoes3.jpg" width='80%'></img>
+          <h3>신발3</h3>
+          <span>태그</span>
+          <p>가격</p>
+        </Col>
+      </Row>
     </div>
   )
 }
