302e261302f16a7ba2797cb7d05205c0d82067f9
hyojin jo <mlhj6010@naver.com>
2022-08-10 22:08:38 +0900

    컴포넌트 만들고 반복문 축약(map)사용해서 간단하게 줄였음

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import shose from './data.js';
 function App() {
 
   let [shoes] = useState(shose)
-  console.log(shoes[0].title)
+  let [img, setimg] = useState(['https://codingapple1.github.io/shop/shoes1.jpg','https://codingapple1.github.io/shop/shoes2.jpg','https://codingapple1.github.io/shop/shoes3.jpg'])
 
   return (
     <div>
@@ -26,26 +26,18 @@ function App() {
       <div className='main_bg' style={{ backgroundImage: 'url(' + bg + ')' }}></div>
       
       <Row>
-        <Col xs={12} md={4}>
-          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width='80%' alt="상품"></img>
-          {/* public 폴더에서 이미지 가져올 때, 권장사항 */}
-          {/* <img src={ process.env.PUBLIC_URL + '/img/logo192.png'} width='80%'></img> */}
-          <h3>{shoes[0].title }</h3>
-          <span>{shoes[0].content }</span>
-          <p>{shoes[0].price }</p>
-        </Col>
-        <Col xs={12} md={4}>
-          <img src="https://codingapple1.github.io/shop/shoes2.jpg" width='80%' alt="상품"></img>
-          <h3>{shoes[1].title }</h3>
-          <span>{shoes[1].content }</span>
-          <p>{shoes[1].price }</p>
-        </Col>
-        <Col xs={12} md={4}>
-          <img src="https://codingapple1.github.io/shop/shoes3.jpg" width='80%' alt="상품"></img>
-          <h3>{shoes[1].title }</h3>
-          <span>{shoes[1].content }</span>
-          <p>{shoes[1].price }</p>
-        </Col>
+        {
+          shose.map(function (a, i) { 
+            return (
+              <Col xs={12} md={4}>
+              <img src={img[i]} width='80%' alt="상품"></img>
+              <h3>{shoes[i].title }</h3>
+              <span>{shoes[i].content }</span>
+              <p>{shoes[i].price }</p>
+            </Col>
+            )
+          })
+        }
       </Row>
     </div>
   )
