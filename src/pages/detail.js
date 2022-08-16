4522b806eac0d599682250067b73f6a802637ae8
hyojin jo <mlhj6010@naver.com>
2022-08-16 22:14:10 +0900

    훅에 대해서 배우고 있는 중. 아직 이해는 잘 안됨. 자바스크립트 문법을 공부를 더해야 할듯

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -2,6 +2,7 @@ import '../css/detail.css';
 import { Button } from 'react-bootstrap';
 import { useParams } from 'react-router-dom'; 
 import styled from 'styled-components'
+import { useEffect } from 'react';
 
 let YellowBtn = styled.button`
   background : ${props => props.bg};
@@ -14,19 +15,27 @@ let YellowBtn = styled.button`
 
 function Detail(props) { 
 
+    useEffect(() => {
+        for (var i = 0; i < 10000; i++){
+            console.log(1);
+        }
+    })
+
+
     let { id } = useParams();
     console.log(id);
+    let 찾은상품 = props.shoes.find(x => x.id == id);
 
     return (
         <div className='detail_wrap contents_line'>
             <div className="product_info_top">
                 <div className='product_img'>
-                    <img src={'https://codingapple1.github.io/shop/shoes'+ (props.shoes[id].id) +'.jpg'}></img>
+                    <img src={'https://codingapple1.github.io/shop/shoes'+ (찾은상품.id) +'.jpg'}></img>
                 </div>
                 <div className='porduct_text'>
-                    <h3>{props.shoes[id].title}</h3>
-                    <span>{props.shoes[id].content}</span>
-                    <p>{props.shoes[id].price}</p>
+                    <h3>{찾은상품.title}</h3>
+                    <span>{찾은상품.content}</span>
+                    <p>{찾은상품.price}</p>
                     <Button variant="secondary" size="sm">
                         주문하기
                     </Button>
