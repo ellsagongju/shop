cb2173fc75a62fcc03ed3bf0733b31a0da874175
hyojin jo <mlhj6010@naver.com>
2022-08-26 22:46:02 +0900

    redux store에 state저장

--- src/store.js
+++ src/store.js
@@ -1,5 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, createSlice } from '@reduxjs/toolkit'
+
+let user = createSlice({
+  // 하나의 state 보관
+  name: 'user',
+  initialState: 'kim'
+})
 
 export default configureStore({
-  reducer: { }
+  reducer: {
+    user: user.reducer
+    // 작명 : 스테이트이름.reducer
+   }
 }) 
